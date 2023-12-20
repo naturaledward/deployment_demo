a639c2da6c8d6d2c1897e0fc5975e6d368474f78
edwardlosangeles <edward.n.delacruz@gmail.com>
2023-12-20 06:44:40 -0800

    202312200645

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,37 +3,19 @@
 import React, { useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import {
-  faGithub,
-  faLinkedin,
-  faMedium,
-  faStackOverflow,
-} from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin, faMedium, faStackOverflow, } from "@fortawesome/free-brands-svg-icons";
 import { Box, HStack } from "@chakra-ui/react";
-
 const socials = [
-  {
-    icon: faEnvelope,
-    url: "mailto: hello@example.com",
-  },
-  {
-    icon: faGithub,
-    url: "https://github.com",
-  },
-  {
-    icon: faLinkedin,
-    url: "https://www.linkedin.com",
-  },
-  {
-    icon: faMedium,
-    url: "https://medium.com",
-  },
-  {
-    icon: faStackOverflow,
-    url: "https://stackoverflow.com",
-  },
-];
-
+  { icon: faEnvelope,
+    url: "mailto: hello@example.com", },
+  { icon: faGithub,
+    url: "https://github.com", },
+  { icon: faLinkedin,
+    url: "https://www.linkedin.com", },
+  { icon: faMedium,
+    url: "https://medium.com", },
+  { icon: faStackOverflow,
+    url: "https://stackoverflow.com", }, ];
 const Header = () => {
   const handleClick = anchor => () => {
     const id = `${anchor}-section`;
@@ -42,11 +24,7 @@ const Header = () => {
       // console.log(element)
       element.scrollIntoView({
         behavior: "smooth",
-        block: "start",
-      });
-    }
-  };
-
+        block: "start", }); } };
   return (
     <Box
       position="fixed"
@@ -57,33 +35,26 @@ const Header = () => {
       transitionProperty="transform"
       transitionDuration=".3s"
       transitionTimingFunction="ease-in-out"
-      backgroundColor="#18181b"
-    >
+      backgroundColor="#18181b" >
       <Box color="white" maxWidth="1280px" margin="0 auto">
         <HStack
           px={16}
           py={4}
           justifyContent="space-between"
-          alignItems="center"
-        >
+          alignItems="center" >
           <nav>
             {/* Add social media links based on the `socials` data */}
             <HStack spacing={8}>
               <a href={socials[0].url}>
-                <FontAwesomeIcon icon={socials[0].icon} size="2x" />
-              </a>
+                <FontAwesomeIcon icon={socials[0].icon} size="2x" /> </a>
               <a href={socials[1].url}>
-                <FontAwesomeIcon icon={socials[1].icon} size="2x" />
-              </a>
+                <FontAwesomeIcon icon={socials[1].icon} size="2x" /> </a>
               <a href={socials[2].url}>
-                <FontAwesomeIcon icon={socials[2].icon} size="2x" />
-              </a>
+                <FontAwesomeIcon icon={socials[2].icon} size="2x" /> </a>
               <a href={socials[3].url}>
-                <FontAwesomeIcon icon={socials[3].icon} size="2x" />
-              </a>
+                <FontAwesomeIcon icon={socials[3].icon} size="2x" /> </a>
               <a href={socials[4].url}>
-                <FontAwesomeIcon icon={socials[4].icon} size="2x" />
-              </a>
+                <FontAwesomeIcon icon={socials[4].icon} size="2x" /> </a>
             </HStack>
           </nav>
           <nav>
